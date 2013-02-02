a47f4aae7c9739f65336b79e0ab85605417bc2ba
Alfasado Inc <webmaster@alfasado.jp>
2013-02-02 17:23:47 +0900

    Fixed. Error at Initialize.

--- plugins/RunWorker/lib/MT/App/RunWorker.pm
+++ plugins/RunWorker/lib/MT/App/RunWorker.pm
@@ -15,83 +15,107 @@ sub _worker {
                             unless $app->user->is_superuser;
     return $app->trans_error( 'Permission denied.' )
                             unless $app->validate_magic;
-    my $daemonize = 0;
-    my $sleep     = 5;
-    my $help      = 0;
-    my $load      = 10;
-    my $verbose   = 0;
-    my $scoreboard;
-    my $randomize_jobs = 0;
-    my $trace_objects  = 0;
-    require MT::TheSchwartz;
-    if ( $trace_objects ) {
-        require Devel::Leak::Object;
-        Devel::Leak::Object->import(qw{ GLOBAL_bless });
-    }
-    my $proc_process_table = eval {
-        require Proc::ProcessTable;
-        1;
-    };
-    $@ = undef;
-    my %cfg;
-    $cfg{ verbose }    = $verbose;
-    $cfg{ scoreboard } = $scoreboard;
-    $cfg{ prioritize } = 1;
-    $cfg{ randomize }  = $randomize_jobs;
-    require MT::Bootstrap;
-    require MT;
-    my $mt = MT->new() or die MT->errstr;
-    if ( defined( MT->config( 'RPTProcessCap' ) ) && $proc_process_table ) {
-        my $t = new Proc::ProcessTable;
-        my $rpt_count;
-        foreach my $p ( @{ $t->table } ) {
-            my $cmd = $p->cmndline;
-            if ( ( $cmd =~ /^perl/ && $cmd =~ /run-workers/ ) || 
-                 ( $cmd =~ /^perl/ && $cmd =~ /run-tasks/ ) ||
-                 ( $cmd =~ /^perl/ && $cmd =~ /run-periodic-tasks/ ) ) {
-                $rpt_count += 1;
+
+    require MT::TheSchwartz::FuncMap;
+    require MT::TheSchwartz::Job;
+    require MT::FileInfo;
+    require MT::WeblogPublisher;
+    my $funcmap = MT::TheSchwartz::FuncMap->load( { funcname => 'MT::Worker::Publish' } );
+    return unless $funcmap;
+    my @jobs = MT::TheSchwartz::Job->load( { funcid => $funcmap->funcid, },
+                                           { 'sort' => 'priority',
+                                             direction => 'ascend',
+                                           },
+                                         );
+    my $pub = MT::WeblogPublisher->new;
+    for my $job ( @jobs ) {
+        if ( my $key = $job->uniqkey ) {
+            if ( my $finfo = MT::FileInfo->load( $key ) ) {
+                if ( $pub->rebuild_from_fileinfo( $finfo ) ) {
+                    $job->remove or die $job->errstr;
+                }
             }
         }
-        if ( $rpt_count > MT->config( 'RPTProcessCap' ) ) {
-            $rpt_count = $rpt_count - 1;
-            return "$rpt_count processes already running; cancelling rebuild_queue launch\n";
-        }
     }
-    if ( MT->config( 'RPTFreeMemoryLimit' ) ) {
-        my $limit = MT->config( 'RPTFreeMemoryLimit' );
-        if ( $limit and ! MT::TheSchwartz::_has_enough_swap( $limit ) ) {
-            return
-                "Free memory below RPT limit; cancelling rebuild_queue launch\n";
-        }
-    }
-    if ( MT->config( 'RPTFreeSwapLimit' ) ) {
-        my $swaplimit = MT->config( 'RPTSwapMemoryLimit' );
-        if ( $swaplimit and ! MT::TheSchwartz::_has_enough_swap( $swaplimit ) ) {
-            return
-                "Free swap memory below RPT limit; cancelling rebuild_queue launch\n";
-        }
-    }
-    $mt->{ vtbl }                 = {};
-    $mt->{ is_admin }             = 0;
-    $mt->{ template_dir }         = 'cms';
-    $mt->{ user_class }           = 'MT::Author';
-    $mt->{ plugin_template_path } = 'tmpl';
-    $mt->run_callbacks( 'init_app', $mt );
-    my $client = eval {
-        require MT::TheSchwartz;
-        my $schwartz = MT::TheSchwartz->new( %cfg );
-        no warnings 'once';
-        $TheSchwartz::FIND_JOB_BATCH_SIZE = $load;
-        $schwartz;
-    };
-    if ( ( my $error = $@ ) && $verbose ) {
-        return "Error initializing TheSchwartz: $error\n";
-    }
-    if ( $daemonize && $client ) {
-        $client->work_periodically( $sleep );
-    } else {
-        $client->work_until_done if $client;
-    }
+
+    #     my $daemonize = 0;
+    #     my $sleep     = 5;
+    #     my $help      = 0;
+    #     my $load      = 10;
+    #     my $verbose   = 0;
+    #     my $scoreboard;
+    #     my $randomize_jobs = 0;
+    #     my $trace_objects  = 0;
+    #     require MT::TheSchwartz;
+    #     if ( $trace_objects ) {
+    #         require Devel::Leak::Object;
+    #         Devel::Leak::Object->import(qw{ GLOBAL_bless });
+    #     }
+    #     my $proc_process_table = eval {
+    #         require Proc::ProcessTable;
+    #         1;
+    #     };
+    #     $@ = undef;
+    #     my %cfg;
+    #     $cfg{ verbose }    = $verbose;
+    #     $cfg{ scoreboard } = $scoreboard;
+    #     $cfg{ prioritize } = 1;
+    #     $cfg{ randomize }  = $randomize_jobs;
+    #     require MT::Bootstrap;
+    #     require MT;
+    #     my $mt = MT->new() or die MT->errstr;
+    #     if ( defined( MT->config( 'RPTProcessCap' ) ) && $proc_process_table ) {
+    #         my $t = new Proc::ProcessTable;
+    #         my $rpt_count;
+    #         foreach my $p ( @{ $t->table } ) {
+    #             my $cmd = $p->cmndline;
+    #             if ( ( $cmd =~ /^perl/ && $cmd =~ /run-workers/ ) || 
+    #                  ( $cmd =~ /^perl/ && $cmd =~ /run-tasks/ ) ||
+    #                  ( $cmd =~ /^perl/ && $cmd =~ /run-periodic-tasks/ ) ) {
+    #                 $rpt_count += 1;
+    #             }
+    #         }
+    #         if ( $rpt_count > MT->config( 'RPTProcessCap' ) ) {
+    #             $rpt_count = $rpt_count - 1;
+    #             return "$rpt_count processes already running; cancelling rebuild_queue launch\n";
+    #         }
+    #     }
+    #     if ( MT->config( 'RPTFreeMemoryLimit' ) ) {
+    #         my $limit = MT->config( 'RPTFreeMemoryLimit' );
+    #         if ( $limit and ! MT::TheSchwartz::_has_enough_swap( $limit ) ) {
+    #             return
+    #                 "Free memory below RPT limit; cancelling rebuild_queue launch\n";
+    #         }
+    #     }
+    #     if ( MT->config( 'RPTFreeSwapLimit' ) ) {
+    #         my $swaplimit = MT->config( 'RPTSwapMemoryLimit' );
+    #         if ( $swaplimit and ! MT::TheSchwartz::_has_enough_swap( $swaplimit ) ) {
+    #             return
+    #                 "Free swap memory below RPT limit; cancelling rebuild_queue launch\n";
+    #         }
+    #     }
+    #     $mt->{ vtbl }                 = {};
+    #     $mt->{ is_admin }             = 0;
+    #     $mt->{ template_dir }         = 'cms';
+    #     $mt->{ user_class }           = 'MT::Author';
+    #     $mt->{ plugin_template_path } = 'tmpl';
+    #     $mt->run_callbacks( 'init_app', $mt );
+    #     my $client = eval {
+    #         require MT::TheSchwartz;
+    #         my $schwartz = MT::TheSchwartz->new( %cfg );
+    #         no warnings 'once';
+    #         $TheSchwartz::FIND_JOB_BATCH_SIZE = $load;
+    #         $schwartz;
+    #     };
+    #     if ( ( my $error = $@ ) && $verbose ) {
+    #         return "Error initializing TheSchwartz: $error\n";
+    #     }
+    #     if ( $daemonize && $client ) {
+    #         $client->work_periodically( $sleep );
+    #     } else {
+    #         $client->work_until_done if $client;
+    #     }
+
     my $admin_script = MT->config( 'AdminScript' );
     my $admin_path = MT->config( 'AdminCGIPath' );
     my $query_str = $app->uri_params( mode => 'start_worker',
